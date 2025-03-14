e35a4782395df8cc40a8a59a96dced309eb7dd2f
AWERDdev <kokorami565@gmail.com>
2025-03-15 00:34:53 +0200

    good work

--- TaskMasterJS/client/src/Pages/Intro.jsx
+++ TaskMasterJS/client/src/Pages/Intro.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import NavBar from "../Components/NavBar";
 import SideBar from "../Components/SideBar";
-
+import { ArrowRight } from 'lucide-react';
 function Intro() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -16,7 +16,7 @@ function Intro() {
   };
 
   return (
-    <main className="relative">
+    <main className="relative bg-[#111827] w-screen h-screen">
       <header>
         <NavBar OpenSidebar={OpenSidebar} />
       </header>
@@ -38,8 +38,29 @@ function Intro() {
         <SideBar CloseSidebar={CloseSidebar} />
       </section>
 
-      <section className="SectionOne"></section>
-      <section className="SectionTwo-Cards"></section>
+      <section className="SectionOne ml-22">
+        <div className="container">
+          <h1 className="text-white mt-40 text-center text-[1rem] md:text-[3rem] font-extrabold mb-4">
+            Organize your tasks with <span className="text-blue-500 font-extrabold">TaskMaster</span>
+          </h1>
+          <p className="text-white  text-center text-[1rem]  font-extrabold mb-4">
+            The simple, efficient way to manage your daily tasks, projects, and goals all in one place.
+          </p>
+          <div className="button-container ml-5 flex justify-center gap-10">
+          <button className=" flex  pl-10 gap-5 text-center text-[1rem] w-[13vw] text-black font-bold px-5 py-2 bg-[#1e3a8a] rounded-md hover:bg-blue-500 transition-all duration-300">
+          Get started <ArrowRight />
+          </button>
+            <button className="text-[1rem]  text-white w-[7vw] hover:text-black outline-1 outline-white font-bold px-5 py-2 bg-black rounded-md hover:bg-gray-500 transition-all duration-300">
+                Login
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="SectionTwo-Cards bg-gradient-to-b from-gray-800 via-gray-600 to-gray-100 ">
+        <div className="Card1"></div>
+        <div className="Card2"></div>
+        <div className="Card3"></div>
+      </section>
       <section className="SectionThree"></section>
     </main>
   );
